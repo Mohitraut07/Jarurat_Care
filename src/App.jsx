bb2de59714b514156c28726e7bd2b894c5a1360d
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-08-14 00:16:19 +0530

    chore: Update styling and functionality of number manipulation web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState([0]);
+  const [redoStack, setRedoStack] = useState([]);
+
+  const handleIncrement = ()=>{
+    if(count < 150){
+      setHistory([...history,count+1]);
+      setRedoStack([]);
+      setCount(count+1);
+    }
+  }
+  const handleDecrement = ()=>{
+    if(count > 0){
+      setHistory([...history,count-1]);
+      setRedoStack([]);
+      setCount(count-1);
+    }
+  }
+
+  const handleUndo = ()=>{
+    if(history.length > 1){
+      const prevElement = history[history.length-2];
+      setRedoStack([history[history.length-1],...redoStack]);
+      setHistory(history.slice(0,-1))
+      setCount(prevElement)
+    }
+  }
+
+  const handleRedo = ()=>{
+    if(redoStack.length>0){
+      const nextElement = redoStack[0];
+      setRedoStack(redoStack.slice(1));
+      setHistory([...history,nextElement])
+      setCount(nextElement)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <nav className="Navbar">
+          <h1>Number Manipulation Web App</h1>
+        </nav>
+        <div className="App-header">
+          <div className="output">
+            <progress
+              value={count}
+              max={150}
+              className="progressBar"
+            ></progress>
+            <p>{((count / 150) * 100).toPrecision(3)}%</p>
+          </div>
+          <div className="App-manipulation-controls">
+          <div className="App-controls">
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
+          </div>
+            <button onClick={handleUndo}>Undo</button>
+            <button onClick={handleRedo}>Redo</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
